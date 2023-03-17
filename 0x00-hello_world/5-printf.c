36cccfc5239a39cddb660b20aa45c181f790c998
Delight Sheriff <delightsheriff@gmail.com>
2023-03-17 00:32:57 +0000

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf() - print to standard output, \n - newline
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
