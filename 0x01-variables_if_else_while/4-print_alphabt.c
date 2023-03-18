fef9699fd8a7d3f4ee429fdbe65e68d0294f5a40
Delight Sheriff <delightsheriff@gmail.com>
2023-03-18 01:28:23 +0000

    4-print_alphabet.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphbet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (alphbet[i] != '\0')
+	{
+		if (alphbet[i] == 'e' || alphbet[i] == 'q')
+			continue;
+		putchar(alphbet[i]);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
