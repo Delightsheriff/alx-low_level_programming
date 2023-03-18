8443262dbaf61b119be15c2aea04a5168954b8fe
Delight Sheriff <delightsheriff@gmail.com>
2023-03-18 03:44:10 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
 }
