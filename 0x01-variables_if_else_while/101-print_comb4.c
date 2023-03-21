e6c5db21616a959e7f286064fc01bf79e64bbb3f
Delight Sheriff <delightsheriff@gmail.com>
2023-03-21 00:05:34 +0000

    101-prints

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,37 +2,34 @@
 
 /**
  * main - Entry point
- *
+ *OOA
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i, j, k;
+	int n, m, l;
 
-	for (i = 0 ; i <= 7 ; i++)
+	for (n = 48 ; n < 58 ; n++)
 	{
-		for (j = 0 ; j <= 8 ; j++)
+		for (m = 49 ; m < 58 ; m++)
 		{
-			for (k = 0 ; k <= 9 ; k++)
+			for (l = 50 ; l < 58 ; l++)
 			{
-				if (i >= j || i >= k)
+				if (l > m && m > n)
 				{
-					continue;
-				}
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-
-				if (i != 7 || j != 8 || k != 9)
-				{
-					putchar(',');
-					putchar(' ');
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
