7f709f4003761b2b6e37a2e236f4927905183b51
Delight Sheriff <delightsheriff@gmail.com>
2023-03-22 22:14:35 +0100

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @x: First argument
+ * @y: Second argument
+ *
+ * Description: This function returns the sum of two integers
+ * Return: integer
+ */
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
