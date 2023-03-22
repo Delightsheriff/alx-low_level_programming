7a70e737a25c73eac65e8a53252da1f6fcdadfec
Delight Sheriff <delightsheriff@gmail.com>
2023-03-22 02:20:20 +0100

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_last_digit - Print the last digit
+  * @i: First argument
+  *
+  * Description: This function prints the last digit of numbers
+  * Return: Always the value of x
+  *
+  */
+
+int print_last_digit(int i)
+{
+	int x = i % 10;
+
+	return (x);
+
+}
