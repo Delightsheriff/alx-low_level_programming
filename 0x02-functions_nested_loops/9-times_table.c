8efba8d8d77964e8ecc60c5e04eae3fbdeaf49a2
Delight Sheriff <delightsheriff@gmail.com>
2023-03-22 20:07:19 +0100

    multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the multiplication table
+ *
+ * Description: This function prints the multiplication table
+ * Return: void
+ *
+ */
+
+void times_table(void)
+{
+	int i, j;
+	int table = 9;
+	int max = 9;
+	int product;
+
+	for (i = 0; i <= table; i++)
+	{
+		for (j = 0; j <= max; j++)
+		{
+			product = i * j;
+
+			if (product <= 9)
+			{
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
