91cb1aa1467f683177122f26dd43a60662d741a9
Delight Sheriff <delightsheriff@gmail.com>
2023-03-23 02:11:24 +0100

    fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints fibonacci numbers
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n = 50;
+	int a = 1;
+	int b = 2;
+	int i;
+
+	printf("%d, %d", a, b);
+
+	for (i = 2; i < n; i++)
+	{
+		int c = a + b;
+
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
+
