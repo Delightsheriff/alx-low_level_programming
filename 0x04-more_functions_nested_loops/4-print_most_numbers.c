704098ecd524540b0c01d4f7a3825072b43c00fc
Delight Sheriff <delightsheriff@gmail.com>
2023-03-24 23:26:11 +0100

    most_numbers-1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,17 +10,11 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		if (i == 2 || i == 4)
-		{
-			continue;
-		}
-		else
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
-			i++;
 		}
 	}
 	_putchar('\n');
