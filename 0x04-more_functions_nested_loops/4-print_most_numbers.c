92224dad167d858713abe9c23f5d577621874f73
Delight Sheriff <delightsheriff@gmail.com>
2023-03-24 23:09:17 +0100

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints integers from 0-9
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
