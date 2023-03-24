4162a17ebfd042839bbab875b4c1bb9e6d164eb5
Delight Sheriff <delightsheriff@gmail.com>
2023-03-25 00:44:25 +0100

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: argument to acted upon
+ *
+ * Return - void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
