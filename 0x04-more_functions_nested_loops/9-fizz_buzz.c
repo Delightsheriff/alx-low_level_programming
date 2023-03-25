debf73172688fc17904615271a12e128f61fbeab
Delight Sheriff <delightsheriff@gmail.com>
2023-03-25 02:50:02 +0100

    fizzb

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
-			printf(" ");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", i);
-			printf(" ");
+			printf("%d ", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
