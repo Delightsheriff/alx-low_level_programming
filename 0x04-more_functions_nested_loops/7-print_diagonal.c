88b5337f4d5140a0fc595dcb5c7ab847bbd8c397
Delight Sheriff <delightsheriff@gmail.com>
2023-03-25 01:06:56 +0100

    diagonal-line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,19 +12,19 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
-			if (i == j)
-			{
-				_putchar('\\');
-			}
-			else
-			{
-				_putchar(' ');
-			}
+			_putchar(' ');
 		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
