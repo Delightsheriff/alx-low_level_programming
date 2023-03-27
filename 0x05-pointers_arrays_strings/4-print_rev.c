b88cd23256d7c8802f37f6180b34603e9db22a5f
Delight Sheriff <delightsheriff@gmail.com>
2023-03-27 20:35:55 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints strings n reverse
+ * @s: argument
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
