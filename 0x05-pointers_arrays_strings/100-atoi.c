e83c07c584e50493b1416e6242152cb3558567ae
Delight Sheriff <delightsheriff@gmail.com>
2023-03-28 00:12:27 +0100

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - converts a strint to an integer
+ * @s: argument
+ *
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int num = atoi(s);
+
+	return (num);
+}
