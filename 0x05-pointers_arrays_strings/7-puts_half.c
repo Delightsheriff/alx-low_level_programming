c5ffc00d1790ed04e71846857bac49e3946444e7
Delight Sheriff <delightsheriff@gmail.com>
2023-03-27 23:15:38 +0100

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: argument
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i;
+	int temp = len / 2;
+
+	if (len % 2 != 0)
+	{
+		temp = (len - 1) / 2;
+	}
+
+	for (i = temp; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
