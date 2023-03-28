cdfb2c04334d7cfabd4d7e3b19872292dd90f58a
Delight Sheriff <delightsheriff@gmail.com>
2023-03-28 17:59:52 +0100

    convert string

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,9 +10,33 @@
 
 int _atoi(char *s)
 {
-	int num = atoi(s);
+	int i = 0;
+	int res;
+	int sine;
+	int stop = 0;
 
-	if
+	sine = -1;
+	res = 0;
 
-	return (num);
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sine = sine * -1;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10;
+			res -= (s[i] - '0');
+			stop = 1;
+		}
+		else if (stop == 1)
+			break;
+
+
+		s++;
+	}
+
+	return (res * sine);
 }
