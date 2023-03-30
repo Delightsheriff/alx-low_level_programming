0f47c89bd2a6d31d4f5eea9399db4990c57410bb
Delight Sheriff <delightsheriff@gmail.com>
2023-03-30 07:01:14 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: first argument
+ * @src: second argument
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
