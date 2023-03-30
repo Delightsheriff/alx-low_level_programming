399eb6f688f8e5daaf43c5cec6bc8baa63393f7b
Delight Sheriff <delightsheriff@gmail.com>
2023-03-30 07:09:58 +0100

    append strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strncat - appends two strings
+ * @dest: first argument
+ * @src: second argument
+ * @n: third argumennt
+ *
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
