e88734c1c0dd1ba5a18be350548df7f3b0e64ae2
Delight Sheriff <delightsheriff@gmail.com>
2023-04-05 18:50:08 +0100

    length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length os a string
+ * @s: array of a string
+ *
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
