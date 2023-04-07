464a01014ec1e3be7b2a95f3c52db03a4977eff0
Delight Sheriff <delightsheriff@gmail.com>
2023-04-07 19:28:04 +0100

    infinte add-3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,22 +13,25 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	for (i = 1; i < argc; i++)
-	{
-		if (!isdigit(*argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			sum += atoi(argv[i]);
-		}
-	}
 	if (argc == 1)
-		printf("%d\n", 0);
-
-
-	printf("%d\n", sum);
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
