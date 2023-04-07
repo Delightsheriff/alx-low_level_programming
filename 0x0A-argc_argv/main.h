575f52059669520c80998f98a647c053f839875f
Delight Sheriff <delightsheriff@gmail.com>
2023-04-07 16:20:53 +0100

    newname

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <math.h>
+
+int main (int argc, char *argv[]);
+
+#endif /* MAIN_H */
