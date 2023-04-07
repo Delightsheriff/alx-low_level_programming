ada53a7df5c43b416a096258250915ff3f9dad61
Delight Sheriff <delightsheriff@gmail.com>
2023-04-07 19:17:35 +0100

    infinte add-1

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,11 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(*argv[i]))
+		if (*argv[i] <= 1)
+		{
+			printf("0");
+		}
+		else if (!isdigit(*argv[i]))
 		{
 			printf("Error\n");
 			return (1);
