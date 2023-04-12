83fe4f65e2f793416f232ab8288756c06eafae6d
Delight Sheriff <delightsheriff@gmail.com>
2023-04-12 21:34:50 +0100

    tears

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,47 +1,103 @@
 #include "main.h"
 
 /**
- * strtow - splits a string into two words
- * @str: character
+ * _isspace - checks if a character is a whitespace
+ * @c: a character
  *
- * Return: character
+ * Return: 1 if c is a whitespace, otherwise 0
  */
+int _isspace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+/**
+ * skipspace - skip all spaces and jump to the start of the next word
+ * @str: a string
+ *
+ * Return: a pointer to the beginning of the next word
+ */
+char *skipspace(char *str)
+{
+	while (*str && _isspace(*str))
+		str++;
+	return (str);
+}
+/**
+ * countchars - count the number of characters in the first word of a string
+ * @str: a string
+ *
+ * Return: the length of the word
+ */
+unsigned int countchars(char *str)
+{
+	unsigned int length = 0;
 
+	while (str[length] && !_isspace(str[length]))
+		length++;
+	return (length);
+}
+/**
+ * countwords - count how many words are in a string
+ * @str: a string
+ *
+ * Return: the number of words in the string
+ */
+unsigned int countwords(char *str)
+{
+	unsigned int wc = 0;
+	char state = 0;
+
+	while (*str)
+	{
+		if (_isspace(*str))
+			state = 0;
+		else if (state == 0)
+		{
+			state = 1;
+			wc++;
+		}
+		str++;
+	}
+	return (wc);
+}
+/**
+ * strtow - split a string into words
+ * @str: a string
+ *
+ * Return: an array of words found in the string
+ */
 char **strtow(char *str)
 {
-	int i = 0, j = 0, k = 0;
 	char **words;
+	char *word;
+	unsigned int wc, w, c;
 
-	if (!str || !*str)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-	words = malloc(strlen(str) * sizeof(char *));
-
-	if (!words)
+	word = skipspace(str);
+	if (*word == '\0')
 		return (NULL);
-
-	while (str[i])
+	wc = countwords(word);
+	words = malloc((wc + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	for (w = 0; w < wc; w++)
 	{
-		while (isspace(str[i]))
-			i++;
+		unsigned int length = countchars(word);
 
-		if (!str[i])
-			break;
-		j = i;
-		while (str[j] && !isspace(str[j]))
-			j++;
-		words[k] = malloc((j - i + 1) * sizeof(char));
-		if (!words[k])
+		words[w] = malloc(length + 1);
+		if (words[w] == NULL)
 		{
-			while (--k >= 0)
-				free(words[k]);
+			while (w--)
+				free(words[w]);
 			free(words);
 			return (NULL);
 		}
-		strncpy(words[k], &str[i], j - i);
-		words[k][j - i] = '\0';
-		k++;
-		i = j;
+		for (c = 0; c < length; c++)
+			words[w][c] = word[c];
+		words[w][c] = '\0';
+		word = skipspace(word + length);
 	}
-	words[k] = NULL;
+	words[w] = NULL;
 	return (words);
 }
