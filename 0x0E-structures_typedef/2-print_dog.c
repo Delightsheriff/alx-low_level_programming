22e19e3ea3bafa3f942db4ebc1c8f725ab699751
Delight Sheriff <delightsheriff@gmail.com>
2023-04-18 00:48:55 +0100

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+	printf("Age: %.6f\n", (d->age) ? d->age : 0);
+	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 }
