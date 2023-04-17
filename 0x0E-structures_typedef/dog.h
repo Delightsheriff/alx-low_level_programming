ae6f214825c622460abef976e280e7eb4aa4ac6b
Delight Sheriff <delightsheriff@gmail.com>
2023-04-17 23:44:58 +0100

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MY_STRUCT_H
+#define MY_STRUCT_H
+
+/**
+ * struct dog - Poppy
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
