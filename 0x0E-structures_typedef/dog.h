41ea39808caabd4a8f4cccbbdbff5170e374a930
Delight Sheriff <delightsheriff@gmail.com>
2023-04-18 01:23:48 +0100

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,17 +9,12 @@
  *
  * Description: dog struct
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-/**
- * dog_t - new name
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
