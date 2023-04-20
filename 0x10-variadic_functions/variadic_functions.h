f30aeab7431fe637567c8c5c57bb3023de9de811
Delight Sheriff <delightsheriff@gmail.com>
2023-04-20 23:52:58 +0100

    stSchool

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,4 +14,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct printer - Struct format_types
+ * @symbol: The conversion specifier
+ * @print: The function pointer
+ *
+ */
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+} printer_t;
+
 #endif /* MAIN_H */
