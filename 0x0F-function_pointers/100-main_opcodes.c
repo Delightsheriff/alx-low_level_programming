0a4136b5d04f70de2fa7317e670bc9f48e728d90
Delight Sheriff <delightsheriff@gmail.com>
2023-04-20 13:20:51 +0100

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	int temp_bytes = atoi(argv[1]);
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (temp_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < temp_bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < temp_bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
