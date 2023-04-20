24308ce720232ef8cc891be8c9bbcf3f81a3759a
Delight Sheriff <delightsheriff@gmail.com>
2023-04-20 18:25:49 +0100

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a newline
+ * @separator: a seperator
+ * @n: count
+ *
+ * Return: integer
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	int temp;
+
+	va_start(args, n);
+
+	if (separator == NULL)
+	{
+		printf(" ");
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		temp = va_arg(args, int);
+
+		printf("%d", temp);
+		if (i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+}
