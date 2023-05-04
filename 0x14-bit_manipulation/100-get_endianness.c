6aa39508e94b918c970e8594abc5a9bc1015fd82
Delight Sheriff <delightsheriff@gmail.com>
2023-05-05 00:00:53 +0100

    Endianness-1

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,8 +7,8 @@
  */
 int get_endianness(void)
 {
-	unsigned int x = 0x12345678;
+	unsigned int x = 1;
 	unsigned char *p = (unsigned char *)&x;
 
-	return (*p == 0x78) ? 1 : 0;
+	return ((int)*p);
 }
