1cb11db96bd57467e78e8e12045ee5440c7d63da
Delight Sheriff <delightsheriff@gmail.com>
2023-05-04 22:35:18 +0100

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,6 +10,7 @@
 #include <ctype.h>
 
 int _putchar(char c);
+void test_(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
