81d7c6b02bfd42dce398e679b3f5b8f24e109d54
Delight Sheriff <delightsheriff@gmail.com>
2023-06-16 01:56:27 +0100

    test

--- 0x17-doubly_linked_lists/test.py
+++ 0x17-doubly_linked_lists/test.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+max_num = float("-inf")
+
+
 def is_palindrome(number):
-    return str(number) == str(number)[::-1]
+    number = str(number)
+    reversed_number = number[::-1]
+    return number == reversed_number
 
 
-largest_palindrome = 0
-
-for i in range(999, 99, -1):
-    for j in range(i, 99, -1):
-        product = i * j
-        if is_palindrome(product) and product > largest_palindrome:
-            largest_palindrome = product
-
-# Save the result to the file
-with open("102-result", "w") as file:
-    file.write(str(largest_palindrome))
+for x in range(100, 1000):
+    for y in range(100, 1000):
+        temp = x * y
+        if (is_palindrome(temp)):
+            if (max_num < temp):
+                max_num = temp
+print(max_num)
